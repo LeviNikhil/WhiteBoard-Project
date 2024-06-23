1041837a2a9d53f1c083e09996238044d83fcbdc
LeviNikhil <nikhilkumarjaiswal2946@gmail.com>
2024-06-24 01:47:12 +0530

    Added Toolbar and Board Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Board from "./components/Board";
+import ToolBar from "./components/ToolBar";
 
 function App() {
-  return (<Board />);
+  return (
+    <>
+        <Board />
+        <ToolBar/>
+    </> 
+);
 }
 
 export default App;
