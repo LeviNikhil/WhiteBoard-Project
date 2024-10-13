10a3ec35f291ddd0275636cf008833a779ea8248
LeviNikhil <nikhilkumarjaiswal2946@gmail.com>
2024-10-13 14:33:02 +0530

    Completed the Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Board from "./components/Board";
-import ToolBar from "./components/ToolBar";
+import Toolbar from "./components/Toolbar";
+import Toolbox from "./components/Toolbox";
 import BoardProvider from "./store/BoardProvider";
+import ToolboxProvider from "./store/ToolboxProvider";
 
 function App() {
   return (
     <BoardProvider>
+      <ToolboxProvider>
+        <Toolbar />
         <Board />
-        <ToolBar/>
-    </BoardProvider> 
-);
+        <Toolbox />
+      </ToolboxProvider>
+    </BoardProvider>
+  );
 }
 
 export default App;
